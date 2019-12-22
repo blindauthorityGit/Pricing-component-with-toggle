13ded6b980262444fde7a73891a3598cd2e2a156
Johannes <office@atelierbuchner.at>
2019-12-22 03:15:38 +0100

    Mobile angelegt, doppelte Größe verwendet

--- js/app.js
+++ js/app.js
@@ -6,6 +6,42 @@ let priceTwoAnnual = document.getElementById("priceTwoAnnual");
 let priceThreeAnnual = document.getElementById("priceThreeAnnual");
 let annualy = document.getElementById("annualy");
 let monthly = document.getElementById("monthly");
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+  e = e || window.event;
+
+  if (e.keyCode == "38") {
+    // up arrow
+  } else if (e.keyCode == "40") {
+    // down arrow
+  } else if (e.keyCode == "37") {
+    priceOne.classList.toggle("fade-out");
+    innerCircle.classList.toggle("leftToggle");
+    priceTwo.classList.toggle("fade-out");
+    priceThree.classList.toggle("fade-out");
+    priceOneAnnual.classList.toggle("fade-in");
+    priceTwoAnnual.classList.toggle("fade-in");
+    priceThreeAnnual.classList.toggle("fade-in");
+    monthly.classList.toggle("inactive");
+    annualy.classList.toggle("active");
+    // left arrow
+  } else if (e.keyCode == "39") {
+    e.preventDefault();
+    priceOne.classList.toggle("fade-out");
+    innerCircle.classList.toggle("leftToggle");
+    priceTwo.classList.toggle("fade-out");
+    priceThree.classList.toggle("fade-out");
+    priceOneAnnual.classList.toggle("fade-in");
+    priceTwoAnnual.classList.toggle("fade-in");
+    priceThreeAnnual.classList.toggle("fade-in");
+    monthly.classList.toggle("inactive");
+    annualy.classList.toggle("active");
+    // right arrow
+  }
+}
+
 console.log(priceTwoAnnual);
 toggleBtn.addEventListener("click", function() {
   innerCircle.classList.toggle("leftToggle");
